123637375a951e1dcaf080309aa23bbfe5a55328
Jonathan Swanson <39644589+MrNobodyX3@users.noreply.github.com>
2021-12-30 20:08:04 -0600

    Site Layout (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
-import { Link, Outlet } from "react-router-dom";
+import styles from "./App.module.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <div className={styles.root}>
+      <div className={styles.container}>
         <Outlet />
-      </header>
+      </div>
     </div>
   );
 }
